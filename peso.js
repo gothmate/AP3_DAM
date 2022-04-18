225b9ebe1043e4709ba746a579f8415791223527
Gothmate <gothmate@gmail.com>
2022-04-18 00:58:38 -0300

    feat(AP3): Desenvolvimento de aplicacoes distribuidas moveis - Adicionados Footers a todas as paginas - Adicionado botao de voltar ao topo - Corrigido bug input com virgula - Adicionado .gitignore

--- peso.js
+++ peso.js
@@ -1,25 +1,27 @@
-h = document.querySelector('#altura');
-peso = document.querySelector('#peso');
-gender = document.querySelectorAll('input[name="gender"]')
+let h = document.querySelector('#altura');
+let peso = document.querySelector('#peso');
+let gender = document.querySelectorAll('input[name="gender"]')
 
-man = document.querySelector('#homem')
-woman = document.querySelector('#muher')
+let man = document.querySelector('#homem')
+let woman = document.querySelector('#muher')
 
 function calculaPeso() {
-    h_num = parseFloat(h.value)
+    
+    let h_num = h.value.replace(',', '.')
+    h_num = parseFloat(h_num)
 
     if(gender[0].checked || gender[1].checked){
         
         if(gender[0].checked) {
-            pesoIdeal = (72.7 *h_num) - 58
-            return peso.innerHTML = `Seu peso ideal para um homem de ${h_num}m é ${pesoIdeal.toFixed(2)}Kg.`
+            let pesoIdeal = (72.7 *h_num) - 58
+            h.value = ''
+            return peso.innerHTML = `O peso ideal para um homem de ${h_num}m é ${pesoIdeal.toFixed(2)}Kg.`
         }
         if(gender[1].checked) {
-            pesoIdeal = (62.1 *h_num) - 44.7
-            return peso.innerHTML = `Seu peso ideal para uma mulher de ${h_num}m é ${pesoIdeal.toFixed(2)}Kg.` 
-        } 
-            
-    
+            let pesoIdeal = (62.1 *h_num) - 44.7
+            h.value = ''
+            return peso.innerHTML = `O peso ideal para uma mulher de ${h_num}m é ${pesoIdeal.toFixed(2)}Kg.` 
+        }         
     } else {
         alert("A escolha do gênero é obrigatória.")
     }
--- salario.js
+++ salario.js
@@ -4,12 +4,13 @@ let result = document.getElementById('salario_total')
 
 function calculaSalario() {
     zeraResultado()
+    valor.value.replace(',', '.')
     let salarioBruto = (parseFloat(valor.value) * parseInt(horas.value)).toFixed(2)
     
     result.innerHTML += `<p>Salário Bruto: R$${salarioBruto}</p>`
-    result.innerHTML += `<p>Desconto INSS: R$${calculaINSS(salarioBruto)}</p>`
-    result.innerHTML += `<p>Desconto IR: R$${calculaIR(salarioBruto)}</p>`
-    result.innerHTML += `<p>Desconto Sindicato: R$${calculaSind(salarioBruto)}</p>`
+    result.innerHTML += `<p>Desconto INSS: R$${calculaINSS(salarioBruto).toFixed(2)}</p>`
+    result.innerHTML += `<p>Desconto IR: R$${calculaIR(salarioBruto).toFixed(2)}</p>`
+    result.innerHTML += `<p>Desconto Sindicato: R$${calculaSind(salarioBruto).toFixed(2)}</p>`
     result.innerHTML += `<p>Salário Líquido: R$${(salarioBruto - calculaINSS(salarioBruto) -calculaINSS(salarioBruto) - calculaSind(salarioBruto).toFixed(2)).toFixed(2)}</p>`
     valor.value = ''
     horas.value = ''
